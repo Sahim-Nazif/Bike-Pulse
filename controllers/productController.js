bd12e00490dd39c96e9ab07bd334b4007846c584
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-30 17:03:57 -0400

    update product

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,9 @@ const Product= require('../models/product')
 const formidable=require('formidable')
 const lodash=require('lodash')
 const fs=require('fs')
+
+
+
 const create_product=(req, res)=>{
 
 
@@ -51,9 +54,57 @@ const read=(req, res)=>{
     return res.json(req.product)
 }
 
+const delete_product=(req, res)=>{
+
+    const product=req.product
+    product.remove((err, deleted)=>{
+        if (err){
+            return res.status(400).status({error:'Could not find the product'})
+        }
+        res.json({message:'Producted deleted successfully!'})
+    })
+}
+
+const update_product=(req, res)=>{
+
+
+    const form= new formidable.IncomingForm()
+    form.keepExtensions=true
+    form.parse(req, (err,fields, files)=>{
+
+         if (err){
+             return res.status(400).json({error:'Image could not uploaded'})
+         }
+
+        let product= req.product
+
+         product=lodash.extend(product, fields)
+
+         if (files.photo) {
+              //1kb=1000
+            //1mb= 1000000
+             if (files.photo.size >1000000) {
+                 return res.status(400).json({error:'Image size should be less than 1mb in size !'})
+             }
+             product.photo.data=fs.readFileSync(files.photo.path)
+             product.photo.contentType=files.photo.type
+         }
+         product.save((err, result)=>{
+             if (err){
+                 return res.status(400).json({error:'Product was not created !'})
+             }
+             res.json(result)
+         })
+    })
+
+}
+
 module.exports={
 
     create_product,
     productById,
-    read
+    read,
+    delete_product,
+    update_product
+
 }
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const router=express.Router()
-const {create_product, productById, read}= require('../controllers/productController')
+const {create_product, productById, read, delete_product, update_product}= require('../controllers/productController')
 const {isAdmin, isAuth}= require('../controllers/authController')
 const {userById}=require('../controllers/userController')
 
 router.post('/product/create/:userId',isAdmin, create_product)
-
+router.delete('/product/delete/:productId/:userId',isAdmin, delete_product)
+router.put('/product/update/:productId/:userId', isAdmin, update_product)
 router.get('/product/:productId', read)
 
 router.param('productId', productById)
