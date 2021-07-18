368a8c8edd5928fd527562c9b191907b028405fb
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-18 18:21:29 -0400

    braintree function updated from connet to BraintreeGateway

--- controllers/brainTreeController.js
+++ controllers/brainTreeController.js
@@ -3,9 +3,9 @@ const braintree=require('braintree')
 
 require('dotenv').config()
 
-const gateway=braintree.connect({
+const gateway=new braintree.BraintreeGateway({
     environment:braintree.Environment.Sandbox,
-    merchantId=process.env.BRAINTREE_MERCHANT_ID,
+    merchantId:process.env.BRAINTREE_MERCHANT_ID,
     publicKey:process.env.BRAINTREE_PUBLIC_KEY,
     privateKey:process.env.BRAINTREE_PRIVATE_KEY,
     
--- routes/braintree.js
+++ routes/braintree.js
@@ -2,9 +2,9 @@ const express=require('express')
 const router=express.Router()
 const {isAuth}=require('../controllers/authController')
 const {userById}=require('../controllers/userController')
+const {generateToken}=require('../controllers/brainTreeController')
 
 
-
-router.get('/braintree/getToken/:userId', isAuth, generateToken)
+router.get('/braintree/getToken/:userId', generateToken)
 router.param('userId', userById)
 module.exports=router
