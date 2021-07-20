202c58367d5014a95a71753c201d8b4dfe89ca33
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-19 20:59:32 -0400

    created process payment route

--- routes/braintree.js
+++ routes/braintree.js
@@ -2,9 +2,11 @@ const express=require('express')
 const router=express.Router()
 const {isAuth}=require('../controllers/authController')
 const {userById}=require('../controllers/userController')
-const {generateToken}=require('../controllers/brainTreeController')
+const {generateToken,processPayment}=require('../controllers/brainTreeController')
 
 
 router.get('/braintree/getToken/:userId', generateToken)
+router.get('/braintree/payment/:userId', processPayment)
+
 router.param('userId', userById)
 module.exports=router
