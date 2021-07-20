d539bb2c211d7cd8184909944c4ae9c9209edcad
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-19 21:19:13 -0400

    created processPayment fun in braintreeController

--- controllers/brainTreeController.js
+++ controllers/brainTreeController.js
@@ -23,7 +23,30 @@ const generateToken=(req, res)=>{
 
 }
 
+const proccesPayment=(req, res)=>{
+
+    let nonceFromTheClient=req.body.paymentMethodNonce
+    let amountFromTheClient=req.body.amount
+    //charge
+    let newTransaction=gateway.transaction.sale({
+        amount:amountFromTheClient,
+        paymentMethodNonce:nonceFromTheClient,
+        options:{
+            submitForSettlement:true
+        }
+    },(error, result)=>{
+        if (error) {
+            res.status(500).json(error)
+        } else {
+            res.json(result)
+        }
+    })
+
+
+}
+
 module.exports={
 
-    generateToken
+    generateToken,
+    proccesPayment
 }
