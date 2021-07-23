d4c5f22132d004e6e16a391a4006ed6f2a36ad93
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-22 22:13:00 -0400

    created middleware to save user purchase history

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const router=express.Router()
 const {isAuth}=require('../controllers/authController')
-const {userById}=require('../controllers/userController')
+const {userById, addOrderToUserHistory}=require('../controllers/userController')
 const {createOrder}=require('../controllers/orderController')
 
 
-router.post('/order/create/:userId', createOrder)
+router.post('/order/create/:userId',addOrderToUserHistory, createOrder)
 
 router.param('userId', userById)
 module.exports=router
