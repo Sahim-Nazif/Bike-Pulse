6aa81f52127dd9bdc5f1e09d7e62271f10b49831
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-22 21:39:43 -0400

    created func to save order to db

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,16 @@
-
+const {Order, CartItem}=require('../models/order')
 
 
 const createOrder=(req, res)=>{
 
-    console.log(req.body)
-
+    req.body.order.user=req.profile
+    const order= new Order(req.body.order)
+    order.save((error, data)=>{
+        if (error) {
+            return res.status(400).json('Sorry could not save your order!')
+        }
+        res.json(data)
+    })
 }
 
 
