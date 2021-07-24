ac1a89c9ebf8c64d78fca1528edcd8638e04ce52
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-24 15:31:06 -0400

    created route to list all orders

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const router=express.Router()
-const {isAuth}=require('../controllers/authController')
+const {isAuth, isAdmin}=require('../controllers/authController')
 const {userById, addOrderToUserHistory}=require('../controllers/userController')
-const {createOrder}=require('../controllers/orderController')
+const {createOrder, listOrders}=require('../controllers/orderController')
 const {decreaseQuantity}=require('../controllers/productController')
 
 router.post('/order/create/:userId',addOrderToUserHistory, decreaseQuantity, createOrder)
+router.get('/order/list/:userId', isAdmin,listOrders )
 
 router.param('userId', userById)
 module.exports=router
