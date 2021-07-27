cd7a55930d36e6c0839de9773a24c1351e5fa89b
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-07-26 20:44:30 -0400

    created route to call controller method for enum values

--- routes/order.js
+++ routes/order.js
@@ -2,11 +2,12 @@ const express=require('express')
 const router=express.Router()
 const {isAuth, isAdmin}=require('../controllers/authController')
 const {userById, addOrderToUserHistory}=require('../controllers/userController')
-const {createOrder, listOrders}=require('../controllers/orderController')
+const {createOrder, listOrders,getStatusValues}=require('../controllers/orderController')
 const {decreaseQuantity}=require('../controllers/productController')
 
 router.post('/order/create/:userId',addOrderToUserHistory, decreaseQuantity, createOrder)
 router.get('/order/list/:userId', isAdmin,listOrders )
+router.get('/order/status-values/:userId', isAdmin, getStatusValues)
 
 router.param('userId', userById)
 module.exports=router
